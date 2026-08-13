fa22c52627e3a0511af80de3ca74419a60730ff4
uhohgio <gehrig8552@sdsu.edu>
2026-08-13 08:02:10 -0700

    Adds GET /api/users

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -5,15 +5,25 @@ const { requireAuth } = require("../middleware/auth");
 const router = express.Router();
 
 // GET /api/users
+// returns a list of all users
 router.get("/", requireAuth, async (req, res) => {
   try {
-    const users = await User.find().select("id displayName role");
+    const { _id, displayName, role } = req.query;
+    const filter = {};
+
+    if (_id) {
+        filter._id = _id;
+    }
+
+    const users = await User.find(filter).select("_id displayName role");
+
     return res.status(200).json({ users });
     } catch (err) {
-      return res.status(500).json({
-          error: err.message
-      })
-    }  
+        return res.status(500).json({
+            error: err.message
+        })
+    }
+       
 });
 
 module.exports = router;
