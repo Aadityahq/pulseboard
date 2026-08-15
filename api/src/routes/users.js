8a6d030b21f63bb1e3fc03efe37bd6a54168ada3
uhohgio <gehrig8552@sdsu.edu>
2026-08-14 19:35:30 -0700

    Simplified users.js to more closely fit the scope of the issue

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -5,25 +5,15 @@ const { requireAuth } = require("../middleware/auth");
 const router = express.Router();
 
 // GET /api/users
-// returns a list of all users
 router.get("/", requireAuth, async (req, res) => {
   try {
-    const { _id, displayName, role } = req.query;
-    const filter = {};
-
-    if (_id) {
-        filter._id = _id;
-    }
-
-    const users = await User.find(filter).select("_id displayName role");
-
+    const users = await User.find().select("id displayName role");
     return res.status(200).json({ users });
     } catch (err) {
-        return res.status(500).json({
-            error: err.message
-        })
-    }
-       
+      return res.status(500).json({
+          error: err.message
+      })
+    }  
 });
 
 module.exports = router;
