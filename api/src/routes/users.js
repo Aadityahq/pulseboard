4366ee14db91f4bc10d360a4274b90375cf4cb42
Giovanna Ehrig <74570609+uhohgio@users.noreply.github.com>
2026-08-17 22:22:38 -0700

    Prettier formatting for /routes/users.js

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -9,18 +9,18 @@ const router = express.Router();
 router.get("/", requireAuth, async (req, res) => {
   try {
     const users = await User.find().select("_id displayName role");
-    return res.status(200).json({ 
-        users: users.map(({ _id, displayName, role }) => ({
-            id: _id,
-            displayName,
-            role,
-        })),
+    return res.status(200).json({
+      users: users.map(({ _id, displayName, role }) => ({
+        id: _id,
+        displayName,
+        role,
+      })),
     });
-    } catch (err) {
-        return res.status(500).json({
-            error: err.message
-        })
-    }
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
 });
 
 module.exports = router;
